b7fe53bdd6426e936229d684b5a693bc51f6b2e7
cristsr <stdio55qqeenn>
2020-10-27 19:47:21 -0500

    fixed switch props

--- lib/Switch.d.ts
+++ lib/Switch.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="react" />
 import { ComponentProps } from './types';
-declare const Switch: ({ props }: ComponentProps) => JSX.Element;
+declare const Switch: (props: ComponentProps) => JSX.Element;
 export default Switch;
 //# sourceMappingURL=Switch.d.ts.map
--- src/Switch.tsx
+++ src/Switch.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDynamicFormContext } from './useDynamicFormContext'
 import { ComponentProps } from './types'
 
-const Switch = ({ props }: ComponentProps) => {
+const Switch = (props: ComponentProps) => {
   const components = useDynamicFormContext()
   const Component = components[props.type]
   return <Component {...props} />
